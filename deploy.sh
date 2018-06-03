c5cc17c4af210a77dcb9031b1e47e7e02e8e0c6e
Shobhit Sharma <ishobhitsharma@gmail.com>
2018-06-03 17:29:47 +0200

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ set -e
 echo "Starting deploy to https://shobh.it"
 
 # Get repo up-to-date
+git config --global pull.default current
 git reset --hard HEAD # we don't want the `git checkout` to cause issues
 git pull origin master
 
@@ -11,15 +12,6 @@ git pull origin master
 export JEKYLL_ENV="production"
 bundle install
 bundle exec jekyll build
-
-# Delete old directories (if any)
-rm -rf "/tmp/shobh.it"
-
-# Copy the generated website to the temporary directory
-cp -R "_site/" "/tmp/shobh.it"
-
-# Copy the finished HTML pages to the current directory
-cp -R /tmp/shobh.it/* .
 
 # Assign permissions
 chmod -R a+rX *
